aee7898f2b1511557a4a74983943eacf0169ded5
zohir-era <zohir@erainfotechbd.com>
2022-03-02 21:57:52 +0600

    Second Commit

--- src/main/java/com/sb/azure/demo/SpringbootAzureDemoApplication.java
+++ src/main/java/com/sb/azure/demo/SpringbootAzureDemoApplication.java
@@ -2,12 +2,14 @@ package com.sb.azure.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class SpringbootAzureDemoApplication {
 	
+	@GetMapping("/greet")
 	public String greet() {
 		return "Congratulations! Greetings from Azure Platform";
 	}
